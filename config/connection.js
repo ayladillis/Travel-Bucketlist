20602829b9fb0a69500bac67d6d1149d39872005
Ayla Dillis <aayladillis@gmail.com>
2020-07-25 21:42:14 -0700

    fixing server

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,15 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "password",
-//   database: "travel_list_db"
-// });
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "travel_list_db"
+});
 
-var MYSQL_URL = process.env.JAWSDB_URL || "mysql://xvfddo1g84yd1cjj:l7yqfbsuozppv0xp@x40p5pp7n9rowyv6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/cxkauw9r1gdr2q0n";
+// var MYSQL_URL = process.env.JAWSDB_URL || "mysql://xvfddo1g84yd1cjj:l7yqfbsuozppv0xp@x40p5pp7n9rowyv6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/cxkauw9r1gdr2q0n";
 // for Heroku
 var connection = mysql.createConnection(MYSQL_URL);
 
