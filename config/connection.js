bcf75892dff48b4941f794e51f74d53d9f5f950a
Ayla Dillis <aayladillis@gmail.com>
2020-07-26 15:55:30 -0700

    edit connection

--- config/connection.js
+++ config/connection.js
@@ -13,6 +13,10 @@ if (process.env.JAWSDB_URL) {
   });
 };
 
+var MYSQL_URL = process.env.JAWSDB_URL || 
+"mysql://xvfddo1g84yd1cjj:oumfso0g3r6dt5fo@x40p5pp7n9rowyv6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/cxkauw9r1gdr2q0n";
+
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
