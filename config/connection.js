57b5bbdfef516d8627790bf4c14de2beac9b87b1
Ayla Dillis <aayladillis@gmail.com>
2020-07-26 15:38:24 -0700

    edit

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,8 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else{
   connection = mysql.createConnection({
-    hose: 'localhost',
+    host: 'localhost',
+    port: 3306,
     user: 'root',
     password: 'password',
     database: 'travel-bucketlist_db'
