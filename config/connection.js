c48a3226454661bb9b673d02c298d6c2d9482378
Ayla Dillis <aayladillis@gmail.com>
2020-07-26 16:06:10 -0700

    bugs

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,8 @@ var mysql = require('mysql');
 var connection;
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(MYSQL_URL);
+  // connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else{
   connection = mysql.createConnection({
     host: 'localhost',
