ee5a69020b9248c4a966aacbe706324bdfe24815
Ayla Dillis <aayladillis@gmail.com>
2020-07-26 15:02:54 -0700

    rerdoing server connection

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,26 @@
-// Set up MySQL connection.
-var mysql = require("mysql");
+var mysql = require('mysql');
+var connection;
 
-
-connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "travel_list_db"
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
+    hose: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'travel-bucketlist_db'
   });
+};
 
-// var MYSQL_URL = process.env.JAWSDB_URL || "mysql://xvfddo1g84yd1cjj:l7yqfbsuozppv0xp@x40p5pp7n9rowyv6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/cxkauw9r1gdr2q0n";
-// var MYSQL_URL = process.env.JAWSDB_URL || "mysql://localhost:3306/cxkauw9r1gdr2q0n";
 
-var connection = mysql.createConnection(MYSQL_URL);
-
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-// Export connection for our ORM to use.
+connection.connect();
 module.exports = connection;
+
+
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
--- db/seeds.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO travel (city_name) VALUES ('NYC');
-
-INSERT INTO travel (city_name, fly) VALUES ('Portland', true);
